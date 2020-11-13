8fca4cb5c1d37535dfdb34945a94e2c898851e27
donnieberry <donniejrberry@gmail.com>
2020-11-13 13:54:52 +0000

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-
+$('#hamburger_icon').on('click', function () {
+    $('.hamburger').toggleClass('activeMenu');
+})
 })
 
 $(document).scroll(function(){
      $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
          $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
  });
+
+
+$(document).ready(function(){
+	$('#hamburger_icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
