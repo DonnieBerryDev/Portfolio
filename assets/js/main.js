2d58bf4209af090412cdccef62b276cd447a95a0
donnieberry <donniejrberry@gmail.com>
2020-12-27 02:06:42 +0000

    Portfolio page changes, not mobile friendly yet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,15 @@
 $(document).scroll(function () {
-    $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
-    $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
+  $("nav").toggleClass("navScrolled", $(this).scrollTop() > 1);
+  $("nav").toggleClass("navScrolled", $(this).scrollTop() > 1);
 });
 $(document).ready(function () {
-    $('#hamburger_icon').on('click', function () {
-        $('.hamburger').toggleClass('activeMenu');
-    })
-})
-
-
-
+  $("#hamburger_icon").on("click", function () {
+    $(".hamburger").toggleClass("activeMenu");
+  });
+});
 
 $(document).ready(function () {
-    $('#hamburger_icon').click(function () {
-        $(this).toggleClass('open');
-    });
+  $("#hamburger_icon").click(function () {
+    $(this).toggleClass("open");
+  });
 });
--- dist/all.js
+++ dist/all.js
@@ -1,16 +1,16 @@
 "use strict";
 
 $(document).scroll(function () {
-  $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
-  $('nav').toggleClass('navScrolled', $(this).scrollTop() > 1);
+  $("nav").toggleClass("navScrolled", $(this).scrollTop() > 1);
+  $("nav").toggleClass("navScrolled", $(this).scrollTop() > 1);
 });
 $(document).ready(function () {
-  $('#hamburger_icon').on('click', function () {
-    $('.hamburger').toggleClass('activeMenu');
+  $("#hamburger_icon").on("click", function () {
+    $(".hamburger").toggleClass("activeMenu");
   });
 });
 $(document).ready(function () {
-  $('#hamburger_icon').click(function () {
-    $(this).toggleClass('open');
+  $("#hamburger_icon").click(function () {
+    $(this).toggleClass("open");
   });
 });
