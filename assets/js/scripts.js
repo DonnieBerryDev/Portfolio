153ab9d4ffd8de34bd234561953306dff75a4e00
Donnie Berry <74161914+DonnieBerryDev@users.noreply.github.com>
2021-07-20 22:54:04 +0100

    Added Express Mortgages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,35 +1,35 @@
-  AOS.init({
-      duration: 1000,
-      offset: 200,
-  });
+AOS.init({
+  duration: 1000,
+  offset: 200,
+});
 
-  const nav = document.querySelector('nav')
-  const hamburger = document.querySelector('#hamburger')
-  const navLinks = document.querySelectorAll('nav a')
-  const showMore = document.querySelector('.hero_showmore')
-  const doc = document.querySelector('html')
-
-  function scrollEvent() {
-      if (window.scrollY > 0) {
-          nav.classList.add('active-nav')
-      } else {
-          nav.classList.remove('active-nav')
-      }
+const nav = document.querySelector("nav");
+const hamburger = document.querySelector("#hamburger");
+const navLinks = document.querySelectorAll("nav a");
+const showMore = document.querySelector(".hero_showmore");
+const doc = document.querySelector("html");
+function scrollEvent() {
+  if (window.scrollY > 0) {
+    nav.classList.add("active-nav");
+    showMore.classList.add("fadeOutHide");
+    console.log(showMore);
+  } else {
+    nav.classList.remove("active-nav");
   }
+}
 
-  navLinks.forEach(link => {
-      link.addEventListener('click', function () {
-          nav.classList.remove('nav-open')
-          document.body.classList.remove('disableScroll')
-          html.classList.remove('disableScroll')
-      })
-  })
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    nav.classList.remove("nav-open");
+    document.body.classList.remove("disableScroll");
+    html.classList.remove("disableScroll");
+  });
+});
 
-  hamburger.addEventListener('click', function () {
-      nav.classList.toggle('nav-open')
-      document.body.classList.toggle('disableScroll')
-      html.classList.toggle('disableScroll')
+hamburger.addEventListener("click", function () {
+  nav.classList.toggle("nav-open");
+  document.body.classList.toggle("disableScroll");
+  html.classList.toggle("disableScroll");
+});
 
-  })
-
-  document.addEventListener('scroll', scrollEvent)
+document.addEventListener("scroll", scrollEvent);
